38625caa5dcded71ec5789e623748582a1ac3c27
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-06-08 00:28:16 -0300

    Get items API ECOLETA

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -1,22 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Constants from 'expo-constants';
 import { Feather as Icon } from '@expo/vector-icons';
 import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import MapView, { Marker } from 'react-native-maps';
 import { SvgUri } from 'react-native-svg';
+import api from '../../services/api'
+
+interface Item {
+    id: number;
+    title: string;
+    image_url: string;
+}
 
 const Points = () => {
+    /* STATES */
+    const [items, setItems] = useState<Item[]>([]);
+    const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
+    /* END STATES */
 
     const navigation = useNavigation();
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            console.log(response.data);
+            setItems(response.data);
+        })
+    }, []);
+
 
     function handleNavigationBack() {
         navigation.goBack();
     }
 
-    function handleNavigateToDetail(){
+    function handleNavigateToDetail() {
         navigation.navigate('Detail');
     }
+
+
+    function hundleSelectItem(id: number){
+        const alreadySelected = selectedItems.findIndex(item => item === id);
+
+        if(alreadySelected >= 0){
+            const filteredItems = selectedItems.filter(item => {
+                return item !== id;
+            });
+
+            setSelectedItems(filteredItems);
+        }else{
+            setSelectedItems([...selectedItems, id]);
+        } 
+    } 
 
     return (
         <>
@@ -59,28 +94,25 @@ const Points = () => {
                     showsHorizontalScrollIndicator={false}
                     contentContainerStyle={{ paddingHorizontal: 20 }}
                 >
-                    <TouchableOpacity style={styles.item} >
-                        <SvgUri width={42} height={42} uri="http://192.168.100.27:3333/uploads/lampadas.svg" />
-                        <Text style={styles.itemTitle}>Lâmpadas</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity style={styles.item} >
-                        <SvgUri width={42} height={42} uri="http://192.168.100.27:3333/uploads/lampadas.svg" />
-                        <Text style={styles.itemTitle}>Lâmpadas</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity style={styles.item} >
-                        <SvgUri width={42} height={42} uri="http://192.168.100.27:3333/uploads/lampadas.svg" />
-                        <Text style={styles.itemTitle}>Lâmpadas</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity style={styles.item} >
-                        <SvgUri width={42} height={42} uri="http://192.168.100.27:3333/uploads/lampadas.svg" />
-                        <Text style={styles.itemTitle}>Lâmpadas</Text>
-                    </TouchableOpacity>
+                   
+                    {items.map(item => (
+                        <TouchableOpacity
+                            key={String(item.id)}
+                            style={[
+                                styles.item,
+                                selectedItems.includes(item.id) ? styles.selectedItem : {}
+                            ]}
+                            activeOpacity={0.6}
+                            onPress={() => hundleSelectItem(item.id)}
+                        >
+                            <SvgUri width={42} height={42} uri={item.image_url} />
+                            <Text style={styles.itemTitle}>{item.title}</Text>
+                        </TouchableOpacity>
+                    ))}
                 </ScrollView>
             </View>
         </>
+
 
     )
 }
